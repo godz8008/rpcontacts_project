869b01e6660532824baadc25a9d88c75b7ed2bc7
Kim Shin <godz8008@gmail.com>
2021-05-17 22:34:24 +0900

    .

--- rpcontacts/database.py
+++ rpcontacts/database.py
@@ -4,7 +4,21 @@
 """This module provides a database connection."""
 
 from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+
+def _createContactsTable():
+    """Create the contacts table in the database."""
+    createTableQuery = QSqlQuery()
+    return createTableQuery.exec(
+        """
+        CREATE TABLE IF NOT EXISTS contacts (
+            id INTEGER PRIMATY KEY AUTOINCREMENT UNIQUE NOT NULL,
+            name VARCHAR(40) NOT NULL,
+            job VARCHAR(50),
+            email VARCHAR(40) NOT NULL
+        )
+        """
+    )
 
 def createConnection(databaseName):
     """Create and open a database connection."""
@@ -19,4 +33,5 @@ def createConnection(databaseName):
         )
         return False
 
+    _createContactsTable()
     return True
